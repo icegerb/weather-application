1dd18abc3d694ef67629ab2c0c4ecaf4ab064de6
Rebecca Yang <jonreb.k@hotmail.com>
2025-02-12 00:01:04 +1000

    config to deploy by github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/weather-application",
+  define: {
+    "import.meta.env.VITE_WEATHER_API_KEY": JSON.stringify(
+      process.env.VITE_WEATHER_API_KEY,
+    ),
+  },
+});
